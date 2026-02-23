51b1d0c38f081ed205118af48a210fdb95eb5cfc
SRINATH GATLA <151995312+Srinath-Gatla@users.noreply.github.com>
2026-02-23 18:23:48 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include "GPIO.h"
 int main(void)
 {
+	uint8_t count;
 	Init_CLK(0);
 	Init_CLK(2);
-	Init_GPIO_MODE(MY_GPIOA,5,OUTPUT_MODE);
+	for(uint32_t i=0;i<8;i++)
+	{
+		Init_GPIO_MODE(MY_GPIOA,i,OUTPUT_MODE);
+	}
 	Init_GPIO_MODE(MY_GPIOC,13,INPUT_MODE);
 	SET_PULLUP_PULLDOWN(MY_GPIOC,MY_PULL_UP,13);
 	while(1)
 	{
-		if(!(GPIO_IDR(MY_GPIOC) & (1<<13)))
+		if(!(GPIO_IDR(MY_GPIOC)& (1<<13)))
 		{
-			MY_SET_BIT(MY_GPIOA,5);
-		}
-		else
-		{
-			MY_CLEAR_BIT(MY_GPIOA,5);
+			count++;
+			CLEAR_BITS(MY_GPIOA,0xFF);
+			SET_BITS(MY_GPIOA,count);
+			for(uint32_t i=0;i<2000000;i++);
 		}
 	}
 }
